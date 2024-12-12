bbff173260b114e966457a2cd0c3b1d9ad718f6c
Soumil <soumil009@outlook.com>
2024-12-12 22:51:41 +0000

    finished day three

--- three/src/main.rs
+++ three/src/main.rs
@@ -1,3 +1,44 @@
+use std::fs;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").unwrap();
+    println!("part one: {}", one(&input));
+    println!("part two: {}", two(&input));
+}
+
+fn one(input : &str) -> i32 {
+    let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    let matches = re.find_iter(input);
+    let mut sum = 0;
+    for mul in matches {
+        sum += parse_multiply(&mul.as_str());
+    }
+    sum
+}
+
+fn two(input : &str) -> i32 {
+    let re = Regex::new(r"mul\(\d+,\d+\)|don't\(\)|do\(\)").unwrap();
+    let matches = re.find_iter(input);
+    let mut mul_enabled = true;
+    let mut sum = 0;
+    for opperator in matches {
+        if opperator.as_str() == "don't()" {
+            mul_enabled = false;
+        } else if opperator.as_str() == "do()" {
+            mul_enabled = true;
+        } else if mul_enabled {
+            sum += parse_multiply(&opperator.as_str());
+        }
+    }
+
+    sum
+}
+
+fn parse_multiply(input : &str) -> i32 {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let caps = re.captures(input).unwrap();
+    let num1 = caps[1].parse::<i32>().unwrap();
+    let num2 = caps[2].parse::<i32>().unwrap();
+    num1 * num2
 }
